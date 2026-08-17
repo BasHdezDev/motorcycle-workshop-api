4dee3872ad1fb25c4012dd8ea99bd6775c2da5ea
Sebastian <bysebashdez@gmail.com>
2026-08-16 23:58:49 -0500

    ✨ create driver module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { DriverModule } from './driver/driver.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
   }),
-    PrismaModule
+    PrismaModule,
+    DriverModule
   ],
   controllers: [AppController],
   providers: [AppService],
