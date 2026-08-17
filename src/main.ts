a55f1c38f17daf5a2ca5146a0feeed7a2a5c2a9e
Sebastian <bysebashdez@gmail.com>
2026-08-17 00:40:47 -0500

    ✨ enable global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,21 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule, 
+    AppModule,
     new FastifyAdapter()
   );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
