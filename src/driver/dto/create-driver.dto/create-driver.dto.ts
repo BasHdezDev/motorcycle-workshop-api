6d762c7e1f42f325db5aba2bd456ccd01b5098de
Sebastian <bysebashdez@gmail.com>
2026-08-17 00:37:14 -0500

    ✨ driver -> added validations with class-validator

--- src/driver/dto/create-driver.dto/create-driver.dto.ts
+++ src/driver/dto/create-driver.dto/create-driver.dto.ts
@@ -1,7 +1,28 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export class CreateDriverDto {
+  @IsString()
+  @IsNotEmpty()
   firstName!: string;
+
+  @IsString()
+  @IsNotEmpty()
   lastName!: string;
+
+  @IsString()
+  @IsNotEmpty()
   documentNumber!: string;
+
+  @IsString()
+  @IsNotEmpty()
   phone!: string;
+
+  @IsOptional()
+  @IsEmail()
   email?: string;
 }
