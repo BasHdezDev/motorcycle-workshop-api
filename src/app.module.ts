f72c5abd791e3796b679fc437233643fd80a9c8e
Sebastian <bysebashdez@gmail.com>
2026-09-13 16:54:22 -0500

    ⚰️ V2 -> Remove dead imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,6 @@ import { DriverModule } from './driver/driver.module';
 import { MotorcycleModule } from './motorcycle/motorcycle.module';
 import { ServiceOrderModule } from './service-order/service-order.module';
 import { HealthModule } from './health/health.module';
-import { HealthV2Controller } from './v2/health/health-v2.controller';
-import { DriverV2Controller } from './v2/driver/driver-v2.controller';
-import { MotorcycleV2Controller } from './v2/motorcycle/motorcycle-v2.controller';
-import { ServiceOrderV2Controller } from './v2/service-order/service-order-v2.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({
