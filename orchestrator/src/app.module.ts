342e7bd41df8e83a337a88899b721645941fce49
Sebastian <bysebashdez@gmail.com>
2026-09-19 18:11:30 -0500

    ✨ Orchestrator -> Add Healtcheck without api key

--- orchestrator/src/app.module.ts
+++ orchestrator/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { InteropController } from './interop/interop.controller';
 import { MetricsController } from './interop/metrics.controller';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [InteropController, MetricsController],
+  controllers: [InteropController, MetricsController, HealthController],
 })
 export class AppModule { }
